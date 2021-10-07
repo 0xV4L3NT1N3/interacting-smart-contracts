0b68528c4d7d8c93b68fa09275ea7d9f50c873c7
V4L3NT1N3 <nicholaschn@protonmail.com>
2021-10-07 18:05:32 +0800

    Added comments

--- script.js
+++ script.js
@@ -2,27 +2,36 @@ const fetch = require('node-fetch');
 const ethers = require("ethers");
 
 async function main() {
+
+    // make an API call to the ABIs endpoint 
     const response = await fetch('https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address=0xC1dcBB3E385Ef67f2173A375F63f5F4361C4d2f9&apikey=YourApiKeyToken');
     const data = await response.json();
 
     let abi = data.result;
     console.log(abi);
 
+    // creating a new Provider, and passing in our node URL
     const node = YOUR_NODE_URL;
     const provider = new ethers.providers.WebSocketProvider(node);
 
+    // initiating a new Wallet, passing in our private key to sign transactions
     let privatekey = YOUR_PRIVATE_KEY;
     let wallet = new ethers.Wallet(privatekey, provider);
 
+    // print the wallet address
     console.log("Using wallet address " + wallet.address);
 
-    let address = "0xC1dcBB3E385Ef67f2173A375F63f5F4361C4d2f9";
-    let contract = new ethers.Contract(address, abi, wallet);
+    // specifying the deployed contract address on Ropsten
+    let contractaddress = "0xC1dcBB3E385Ef67f2173A375F63f5F4361C4d2f9";
 
+    // initiating a new Contract
+    let contract = new ethers.Contract(contractaddress, abi, wallet);
+
+    // calling the "retrieve" function to read the stored value
     let read = await contract.retrieve();
     console.log("Value stored in contract is " + read.toString());
 
-    // call the "store" function to update the value to 420
+    // calling the "store" function to update the value to 420
     let write = await contract.store(420);
     
     // wait for 2 blocks of confirmation 

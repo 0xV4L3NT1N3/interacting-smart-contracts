c4a7aa36b55f58bb0b69a13567ffac615da46659
0xV4L3NT1N3 <nicholaschn@protonmail.com>
2022-12-09 01:42:59 +0800

    Updated contract addresses to Sepolia

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ethers = require("ethers");
 async function main() {
 
     // make an API call to the ABIs endpoint 
-    const response = await fetch('https://api-ropsten.etherscan.io/api?module=contract&action=getabi&address=0xC1dcBB3E385Ef67f2173A375F63f5F4361C4d2f9&apikey=YourApiKeyToken');
+    const response = await fetch('https://api-sepolia.etherscan.io/api?module=contract&action=getabi&address=0xFc7a5BD22dFc48565D6f04698E566Dd0C71d3155&apikey=YourApiKeyToken');
     const data = await response.json();
 
     let abi = data.result;
@@ -11,33 +11,33 @@ async function main() {
 
     // creating a new Provider, and passing in our node URL
     const node = YOUR_NODE_URL;
-    const provider = new ethers.providers.WebSocketProvider(node);
+    const provider = new ethers.providers.JsonRpcProvider(node);
 
     // initiating a new Wallet, passing in our private key to sign transactions
-    let privatekey = YOUR_PRIVATE_KEY;
+    let privatekey = Your_PRIVATE_KEY;
     let wallet = new ethers.Wallet(privatekey, provider);
 
     // print the wallet address
     console.log("Using wallet address " + wallet.address);
 
     // specifying the deployed contract address on Ropsten
-    let contractaddress = "0xC1dcBB3E385Ef67f2173A375F63f5F4361C4d2f9";
+    let contractaddress = "0xFc7a5BD22dFc48565D6f04698E566Dd0C71d3155";
 
     // initiating a new Contract
     let contract = new ethers.Contract(contractaddress, abi, wallet);
 
     // calling the "retrieve" function to read the stored value
-    let read = await contract.retrieve();
+    let read = await contract.read();
     console.log("Value stored in contract is " + read.toString());
 
     // calling the "store" function to update the value to 420
-    let write = await contract.store(420);
+    let write = await contract.write(1337);
     
     // wait for 2 blocks of confirmation 
     write.wait(2)
         .then(async () => {  
             // read the contract again, similar to above
-            let read = await contract.retrieve();
+            let read = await contract.read();
             console.log("Updated value stored in contract is " + read.toString());
         });
 }
